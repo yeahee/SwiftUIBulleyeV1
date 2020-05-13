f0770b2ff89c9134723f0658fb85ece1f13c1e26
yeahee <4023yhk@naver.com>
2020-05-13 14:16:20 +0900

    Build Basic User Interface

--- SwiftUIBulleyeV1/ContentView.swift
+++ SwiftUIBulleyeV1/ContentView.swift
@@ -10,23 +10,55 @@ import SwiftUI
 
 struct ContentView: View {
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     var body: some View {
         VStack {
-            Text("Welcome to my first App!")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
-            Button(action: {
-                print("Button pressed!")
-                self.alertIsVisible = true
-            }) {
-                Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+            VStack {
+                Spacer()
+                // Target row
+                HStack{
+                    Text("Put the bullseye as close as you can to:")
+                    Text("100")
+                }
+                Spacer()
+                // Slider row
+                HStack{
+                    Text("1")
+                    Slider(value: self.$sliderValue, in: 1...100)
+                    Text("100")
+                }
+                Spacer()
+                // Button row
+                Button(action: {
+                    print("Button pressed!")
+                    self.alertIsVisible = true
+                }) {
+                    Text(/*@START_MENU_TOKEN@*/"Hit me!"/*@END_MENU_TOKEN@*/)
+                }
+                // State for alert
+                    .alert(isPresented: self.$alertIsVisible){
+                        Alert(title: Text("Hello there!"),
+                              message: Text("The slider's value is \(Int(self.sliderValue.rounded()))."),
+                              dismissButton: .default(Text("Awesome!")))
+                }// End of alert
+                Spacer()
+                // Score row
+                HStack{
+                    Button(action: {}){
+                        Text("Start Over")
+                    }
+                    Spacer()
+                    Text("Score:")
+                    Text("999999")
+                    Spacer()
+                    Text("Round:")
+                    Text("999")
+                    Spacer()
+                    Button(action:{}){
+                        Text("Inform")
+                    }
+                }.padding(.bottom, 20)
             }
-            // State for alert
-                .alert(isPresented: self.$alertIsVisible){
-                    Alert(title: Text("Hello there!"),
-                          message: Text("This is my first pop-up."),
-                          dismissButton: .default(Text("Awesome!")))
-            }// End of alert
         }// End of VStack
     }// End of body
 }
